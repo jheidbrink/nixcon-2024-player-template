914ea59485b6442c52bdf0e01fccc584e7740e0c
Jan Heidbrink <jan.heidbrink@posteo.de>
2024-10-26 18:44:13 +0200

    add

--- webserver.py
+++ webserver.py
@@ -7,6 +7,10 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
+@app.get("/{a}/{b}")
+async def add(a: int, b: int) -> int:
+    return a + b
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
